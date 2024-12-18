bcb79699335173d11d09c8e4a4817d63092eec73
heisannn <53432535+heisannn@users.noreply.github.com>
2024-12-18 20:00:30 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,3 @@ cp docker-compose.example.yaml docker-compose.yaml
 cp mirakc/config.example.yml mirakc/config.yml
 cp KonomiTV/docker-compose.example.yaml KonomiTV/docker-compose.yaml
 cp KonomiTV/config.example.yaml KonomiTV/config.yaml
-docker compose up -d
